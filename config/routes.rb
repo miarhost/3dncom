e4fa29284b71cd5972318473a21da30b7d7cc3ae
MariaTikhonova <ritz.rkraft@gmail.com>
2017-12-01 13:33:58 +0200

    Added devise users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users # , path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
 root 'landing#home'
   get '/home', to: 'landing#home'
   get '/about', to: 'landing#about'
