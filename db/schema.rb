ce67273e1d4e75fd147524e5ee873e87f49f37c7
MariaTikhonova <ritz.rkraft@gmail.com>
2017-12-19 21:50:30 +0200

    Added frontend content to main page

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,16 @@ ActiveRecord::Schema.define(version: 20171214132211) do
     t.datetime "updated_at", null: false
   end
 
-# Could not dump table "comments" because of following StandardError
-#   Unknown type 'users' for column 'reference'
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "Article"
@@ -47,21 +55,6 @@ ActiveRecord::Schema.define(version: 20171214132211) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "Admin"
-    t.string "name"
-    t.string "password"
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
