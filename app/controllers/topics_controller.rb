17c400b7ae7df83c9d91e320be6eaa3efe0c10ca
MariaTikhonova <ritz.rkraft@gmail.com>
2017-12-25 19:18:58 +0200

    Change new topic addind

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
-  #before_action :set_topic, only: [:show, :edit, :update, :destroy]
-#before_action :authenticate_user!, only:[:edit, :update, :destroy]
+ before_action :set_topic, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, only:[:edit, :update, :destroy]
 
 	def index
 		@topics = Topic.all
