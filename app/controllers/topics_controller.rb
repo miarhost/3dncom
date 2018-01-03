b8d9b18ec4b75ffd3acb89d4ddef4e87a9e0105f
MariaTikhonova <ritz.rkraft@gmail.com>
2018-01-03 14:26:35 +0200

    fixed users names at forum

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,9 +4,7 @@ before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
 	def index
 		@topics = Topic.all
-     #if !user_signed_in?
-  #redirect_to new_user_session_path
-#end
+
 	end
 
 	def show 
@@ -19,6 +17,9 @@ before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
 	def new
 	@topic = Topic.new
+       if !user_signed_in?
+  redirect_to new_user_session_path
+ end
     end
     
     def edit
