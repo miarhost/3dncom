a8abb5d35951ef69788ed34c1f8b6ba5e6219f12
MariaTikhonova <ritz.rkraft@gmail.com>
2018-01-10 19:35:36 +0200

    changed gravatar method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,20 +8,8 @@ module ApplicationHelper
     end
     end
 
-    def gravatar_for(user, size: 25)
-           gravatar_url =  "https://www.gravatar.com/avatar"
-       gravatar_id =  Digest::MD5.hexdigest(user.email)
-       image_tag(gravatar_url, alt: user.name)
-    end
-
-        def gravatar_for_topics(user, size: 10)
-           gravatar_url =  "https://www.gravatar.com/avatar"
-       gravatar_id =  Digest::MD5.hexdigest(user.email)
-       image_tag(gravatar_url, alt: user.name)
-    end
-
-        def gravatar_for_admin(user, size: 25)
-           gravatar_url =  "https://www.gravatar.com/avatar"
+    def gravatar_for(user, size)
+           gravatar_url =  "https://www.gravatar.com/avatar/#{gravatar_url}.png?s=#{size}"
        gravatar_id =  Digest::MD5.hexdigest(user.email)
        image_tag(gravatar_url, alt: user.name)
     end
