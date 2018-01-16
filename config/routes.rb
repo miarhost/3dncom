b4186d19be003b8b0208a1a67411f7c854acbb44
MariaTikhonova <ritz.rkraft@gmail.com>
2018-01-16 14:38:37 +0200

    Rearranged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,47 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :articles do
-  	resources :comments
-  end
-  
-    resources :topics do
-       resources :messages
-    end
 
-  devise_for :admins, controllers: {
+  root 'landing#home'
+  get '/home', to: 'landing#home'
+  get '/about', to: 'landing#about'
+  get '/gallery', to: 'landing#gallery'
+  get 'about(/:articles)', to: 'landing#about'
+  get 'home(/:topics)', to: 'landing#home'
+
+    devise_for :admins, controllers: {
         sessions: 'admins/sessions'
       }
     
   devise_scope :admin do
     get "admins/home"=> "admins/sessions#home", :as => "admin_home"
   end
-  	  devise_for :users do #controllers: {sessions: 'users/sessions'}
+      devise_for :users do #controllers: {sessions: 'users/sessions'}
       #as :user do 
       #get 'signin', to: 'devise/sessions#new', as: :new_user_session
        #end
+         resources :topics
            resources :comments
-  	  	    resourcces :messages
-             resources :topics
-  	      end  # , path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
-root 'landing#home'
-  get '/home', to: 'landing#home'
-  get '/about', to: 'landing#about'
-  get '/gallery', to: 'landing#gallery'
-  get 'about(/:articles)', to: 'landing#about'
-  get 'home(/:topics)', to: 'landing#home'
+            resourcces :messages
+            
+          end # , path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
+
   #get '/users' => 'landing#home', as: :user_root    #temporarily
 
   #devise_scope :user do
     #get 'login', to: 'landing#home' 
  #end
+
+  resources :articles do
+  	resources :comments
+  end
+    
+  resources :topics do
+       resources :messages
+    end
+
+   resources :messages
+
+
+
+
 
 end
