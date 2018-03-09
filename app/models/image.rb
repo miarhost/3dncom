0a71c6de4e772214fc7a63a7b077ca39151a69f4
MariaTikhonova <ritz.rkraft@gmail.com>
2018-03-09 20:22:57 +0200

    carrierwave s3 configs

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ApplicationRecord
 	belongs_to :topic
 
 	
-  has_attached_file :image, :styles => {:thumb => '300x300#'}
+  #has_attached_file :image, :styles => {:thumb => '300x300#'}
 
-validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+#validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
