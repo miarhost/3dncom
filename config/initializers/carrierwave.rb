238d464cd5a01fbc5fcba9dce2f8aafca438de21
MariaTikhonova <ritz.rkraft@gmail.com>
2018-03-14 14:03:33 +0200

    s3 fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["S3_ACCESS_KEY"],                       # required
     aws_secret_access_key: ENV["S3_SECRET_KEY"],                        # required
     region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
-    host:                  's3-eu-west-2.amazonaws.com',             # optional, defaults to nil
-    endpoint:              's3-eu-west-2.amazonaws.com' # optional, defaults to nil
+    host:                  'https://s3.eu-west-2.amazonaws.com',             # optional, defaults to nil
+    endpoint:              'https://s3.eu-west-2.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = ENV["S3_BUCKET"]                                 # required
   config.fog_public     = false                                                 # optional, defaults to true
