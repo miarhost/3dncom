0505c3db1e1cbeea6a67c3319bc59dc2d4d0a22f
MariaTikhonova <ritz.rkraft@gmail.com>
2018-03-20 19:47:56 +0200

    Added 2nd carousel for gallery: meshes

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,15 +1,16 @@
 class SubscriptionsController < ApplicationController
 
-	def new
-		@subscrittion = Subscription.new
-	end
+	#def new
+		#@subscrittion = Subscription.new
+	#end
 
 	def subscribe(email)
 		@submail = email
 		SubscriptionMailer.subscribe(@submail)
 		if @submail.save
-			format.html { render 'subscribe' }
-			format.text { render 'subscribe' }
+			flash[:success] = 'Your subscription is set'
+			#format.html { render 'subscribe' }
+			#format.text { render 'subscribe' }
 		else
 		end
     end
