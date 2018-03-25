1430d78c6da2445336bed0fb2c51c44928b15464
MariaTikhonova <ritz.rkraft@gmail.com>
2018-03-25 23:22:02 +0300

    new resources links, email validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,8 @@
 class Subscription < ApplicationRecord
 	#before_save {self.email = self.email.downcase}
-	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	#validates :email, #presence: true,
-	         #length: { minimum: 12, maximum: 255 },
-	         #format: {with: VALID_EMAIL_REGEX }#,
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, #presence: true,
+	         length: { minimum: 12, maximum: 255 },
+	         format: {with: VALID_EMAIL_REGEX }#,
 	         #uniqueness: { case_sensitive: false }
 end
