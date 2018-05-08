c834da303d42f44ac2ef98c92ac75c4f40226c3b
MariaTikhonova <ritz.rkraft@gmail.com>
2018-05-08 16:38:44 +0300

    Controller tests

--- test/controllers/branches_controller_test.rb
+++ test/controllers/branches_controller_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class BranchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+setup do
+@branch = branches(:one)
+end
+
+test "should get index" do
+ get branches_url 
+ assert_responce :success
+end 
+
+test "should get new branch" do
+	get branch_url(@branch)
+    assert_responce :success
+end
+
+test "should create branch" do
+ assert_difference('Branch.count') do
+  post branches_url, params: { branch: {topicthread: @topicthread.brahch } }
+
+end
+assert_redirected_to branch_url(Branch.last)
+end
+
+test "should show branch" do
+get branch_url(@branch)
+assert_responce :success
+end
+
+test "should get edit" do
+	get edit_branch_url (@branch)
+ assert_responce :success
+end
+
+test "should update branch" do
+	patch branch_url, params: { branch: { topicthread: @topicthread.branch } }
+ assert_redirected_to branch_url(@branch)
+end
+
+test "should destroy branch" do
+	assert_difference('Branch.count', -1) do
+		delete branch_url(@branch)
+	end 
+assert_redirected_to branches_url
+end
+
 end
--- test/controllers/topicthreads_controller_test.rb
+++ test/controllers/topicthreads_controller_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class TopicthreadsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+require 'test_helper'
+
+class TopicthreadsControllerTest < ActionDispatch::IntegrationTest
+
+setup do 
+@topicthread = topicthreads(:one)
+end
+
+test 'should get index' do
+get topicthreads_url 
+assert_responce :success 
+end
+
+test 'should get new' do
+get new_topicthread_url 
+assert_responce :success
+end
+
+test 'should create new topicthread' do
+ assert_difference ('Topictread.count') do
+ 	post topicthread_url, params: {topicthread: { } }
+ end
+ 
+ assert_redirected_to cart_url(Topicthread.last)
+end 
+
+test 'should show topicthread' do
+	get topicthread_url(@topicthread)
+ assert_responce :success
+end 
+
+
+test 'should get edit topicthread' do
+	get edit_topicthread_url(@topicthread)
+assert_responce :success
+end
+
+test 'should update topicthread' do
+  patch topicthread_url(@topicthread), params: { topicthread: { } }
+  assert_redirected_to topicthread_url(@topicthread)
+end
+
+
+test 'should destroy topicthread' do
+assert_difference ('Topicthread.count', -1) do
+delete topicthread_url(@topicthread)
+end
+assert_redirected_to topicthreads_url 
+end
+
 end
